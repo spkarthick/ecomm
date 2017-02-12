c6511ae0dd499f3dad2acb7de809ec8184769075
SP <SP>
2017-02-12 12:03:45 +0530

    tracking update

--- components/track/track.service.js
+++ components/track/track.service.js
@@ -5,7 +5,7 @@
 	module.factory("trackService", ["$http", function($http) {
 	    return {
 	        getStatus: function (orderId) {
-	            return $http.get("http://localhost:7000/status/" + orderId);
+	            return $http.get("http://youngandenergetic.com:7000/status/" + orderId);
 	        }
 	    };
 	}]);
